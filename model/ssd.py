3629342f9e4d10f9c2a5fb697b005b14757ad57b
d-thatcher <d.thatcher100@gmail.com>
2020-02-10 17:07:01 -0800

    ssd

--- model/ssd.py
+++ model/ssd.py
@@ -19,32 +19,22 @@ class SSD(nn.Module):
 
 class Predict(nn.Module):
     def __init__(self, class_num = 80):
-        
+        self.in_planes = [512, 1024, 512, 256, 256, 256]
+        self.anchors = [4, 6, 6, 6, 4, 4]
+        self.class_num = class_num + 1
 
     def forward(self, features):
+        los = []
+        cls = []
+        for i,f in enumerate(features):
+            los.append(nn.Conv2d(self.in_planes[i], self.anchors[i]*4, 3, 1, 1))
+            cls.append(nn.Conv2d(self.in_planes[i], self.anchors[i]*self.class_num+1, 3, 1, 1))
+        
         return         
-        
 
-    def predict(self, features, ):
-        f1, f2, f3, f4, f5, f6 = features
-        
-    def 
         
         
         
-    def classifier(self, in_depth, class_num, anchor_num):
-        """output is the feature n*m*(anchor_number*(class_num+4))
-             anchor_num: the number of anchors in this feature.
-             class_num: the number of class, the output is the possiblity of each class
-                        (0~1) and ssd has one more class which is for background.
-             4: the location variables (cx,cy,width, height) 
-        """
-        out_depth = anchor_num * (class_num + 4)
-        layers = nn.Sequential(
-            nn.Conv2d(in_depth, out_depth, 3, 1, 1),
-            nn.ReLU(input=True)
-        )
-        return layers
 
 cfg = [[4, 1024], [],6,6,]
 
